d3dc81ae249f5300f6f53b604ce1dfd0cd2e67fe
James Roach <roachjweston@gmail.com>
2023-02-14 03:23:44 +0000

    added basic object spawning and movement

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 let jumping = false;
 
+const spawnlist = ['block', 'long block', 'floating block', 'gap'];
+
 window.addEventListener('keydown', event => {
   if (event.code === 'Space') {
     let direction = 'up';
     if (!jumping) {
       jumping = true;
       const jump = setInterval(() => {
-        if (getComputedStyle(document.querySelector('.car')).bottom !== '100px' && direction === 'up') {
-          document.querySelector('.car').style.bottom = (parseInt(getComputedStyle(document.querySelector('.car')).bottom.split('px')[0]) + 1) + 'px';
+        if (getComputedStyle(document.querySelector('.car')).bottom !== '150px' && direction === 'up') {
+          document.querySelector('.car').style.bottom = (parseInt(getComputedStyle(document.querySelector('.car')).bottom.split('px')[0]) + 2) + 'px';
         } else if (getComputedStyle(document.querySelector('.car')).bottom !== '0px' && direction === 'down') {
           document.querySelector('.car').style.bottom = (parseInt(getComputedStyle(document.querySelector('.car')).bottom.split('px')[0]) - 1) + 'px';
-        } else if (getComputedStyle(document.querySelector('.car')).bottom === '100px') {
+        } else if (getComputedStyle(document.querySelector('.car')).bottom === '150px') {
           direction = 'down';
         } else if (getComputedStyle(document.querySelector('.car')).bottom === '0px') {
           direction = 'up';
@@ -21,3 +23,37 @@ window.addEventListener('keydown', event => {
     }
   }
 });
+
+moveObject = object => {
+  const move = setInterval(() => {
+    if (getComputedStyle(object).left !== '-300px') {
+      object.style.left = (parseInt(getComputedStyle(object).left.split('px')[0]) - 2) + 'px';
+    } else {
+      document.querySelector('.player-area').removeChild(object);
+    }
+  }, 4);
+}
+
+spawnObject = () => {
+  const $playerArea = document.querySelector('.player-area');
+  const block = document.createElement('div');
+
+  block.className = spawnlist[Math.floor(Math.random() * spawnlist.length)];
+
+  console.log(spawnlist, block.className);
+
+  $playerArea.appendChild(block);
+
+  moveObject(block);
+}
+
+setInterval(spawnObject, 2500);
+
+setTimeout(() => {
+  setInterval(spawnObject, 2500);
+}, 11250)
+
+setTimeout(() => {
+  spawnlist.push('gap wide');
+  console.log(spawnlist);
+}, 25000)
